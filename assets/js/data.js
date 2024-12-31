9eadc8f6d0f5d08249d6d9e9ed16b94e17593788
arielEliasPerez <ariel65_ep@hotmail.com>
2024-12-31 20:00:16 -0300

    Primera Estructura

---
+++ assets/js/data.js
@@ -0,0 +1,17 @@
+// assets/js/data.js
+const menuData = [
+    { category: "promos", name: "Balde de Corona", description: "6 Coronas de 330 ml", price: 25000 },
+    { category: "promos", name: "Promo Dr Lemon", description: "3 botellas de Dr Lemon. PROMO VÁLIDA HASTA LAS 22:00 DÍAS MIERCOLES, JUEVES, SÁBADOS Y DOMINGOS", price: 20000 },
+    { category: "comidas", name: "Hamburguesa Simple", description: "1 Medallón de carne, lechuga y tomate.", price: 6000 },
+    { category: "comidas", name: "Hamburguesa Mitos", description: "1 Medallón de carne, lechuga, tomate, jamón y queso. Acompañado de papas fritas.", price: 12000 },
+    { category: "cervezas", name: "Cerveza Brahma", description:"Botella de 1 Litro", price: 7000 },
+    { category: "cervezas", name: "Cerveza Corona", description:"Botella de 355 ml", price: 5000 },
+    { category: "bebidas", name: "Chandón Rose/Aperitif/Delise/Extra Brut", description: "Botella 1 Litro con 2 Speed.", price: 36000 },
+    { category: "bebidas", name: "Cosecha Tardía", description: "Botella 1 Litro con 2 Speed", price: 36000 },
+    { category: "tragos", name: "Aperol Spritz", description: "Aperol, Espumante, dash de Tónica", price: 8000 },
+    { category: "tragos", name: "Blue Lagoon", description: "Vodka, Blue Curacao, Zumo de Limón y Sprite", price: 8000 },
+    { category: "sin-alcohol", name: "Agua Mineral", description: "", price: 3000 },
+    { category: "sin-alcohol", name: "Coca-Cola Lata", description: "", price: 3000 },
+    { category: "acompañamientos", name: "Maní Salado", description: "Mini pote de Maní", price: 1000 },
+  ];
+  
