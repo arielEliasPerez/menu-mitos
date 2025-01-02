fec67ce9ff010735eda70836db1ed9fa71942ca6
Ariel <ariel65_ep@hotmail.com>
2025-01-02 01:19:09 -0300

    Agregado de info-container en header

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,7 +1,18 @@
-document.querySelectorAll("#navbar a").forEach(link => {
-    link.addEventListener("click", event => {
-        event.preventDefault();
-        const sectionId = link.getAttribute("href");
-        document.querySelector(sectionId).scrollIntoView({behavior: "smooth"});
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.getElementById("navbar");
+    const infoContainer = document.querySelector(".info-container");
+  
+    // Calcular la altura del infoContainer
+    const navbarOffsetTop = infoContainer.offsetHeight;
+    console.log(infoContainer.offsetHeight);
+    window.addEventListener("scroll", () => {
+      if (window.scrollY >= navbarOffsetTop) {
+        console.log("entre por aqui");
+        navbar.classList.add("fixed"); // Hacer que el navbar sea fijo
+      } else {
+        navbar.classList.remove("fixed"); // Restaurar posición inicial
+        console.log("no lo hice");
+      }
     });
-});
+  });
+  
